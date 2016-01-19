28970b6310c293342232b28fb5d12cbd6982ec1e
Crystal232900 <crystalsphone924@gmail.com>
2016-01-18 17:34:15 -0700

    Initial Files

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloSunshine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloSunshine");
+            Console.ReadLine();
+            Console.WriteLine("didyousleepgood?");
+            Console.ReadLine();
+
+        }
+    }
+}
